e60a8fccac82bf326c7f3f70564463f5ac3b0073
Greebo <greeborox@gmail.com>
2016-04-02 21:36:16 +0200

    lava and moving platforms

--- Actors/platform.js
+++ Actors/platform.js
@@ -5,12 +5,49 @@ platform = entity.newEntity();
 platform.color = "gray";
 
 module.exports = {
-  newPlatform: function(x,y,width,height){
+  newPlatform: function(x,y,width,height,color){
     var newPlat = Object.create(platform);
     newPlat.x = x;
     newPlat.y = y;
     newPlat.width = width;
     newPlat.height = height;
+    if(color){
+      newPlat.color = color;
+    }
     return newPlat;
-  }
+  },
+  newMovingPlatform: function(x,y,width,height,color, maxY, minY, speed){
+    var newPlat = Object.create(platform);
+    newPlat.x = x;
+    newPlat.y = y;
+    newPlat.maxY = maxY;
+    newPlat.minY = minY;
+    newPlat.width = width;
+    newPlat.height = height;
+    newPlat.goingDown = true;
+    if(color){
+      newPlat.color = color;
+    }
+    newPlat.speed = speed;
+    newPlat.update = function(){
+      if(this.y >= this.maxY) {
+        this.y = this.maxY;
+      }
+      if(this.y <= this.minY) {
+        this.y = this.minY;
+      }
+      if(this.y === this.maxY && this.goingDown === true) {
+        this.goingDown = false;
+      }
+      if(this.y === this.minY && this.goingDown === false) {
+        this.goingDown = true;
+      }
+      if(this.goingDown) {
+        this.y += this.speed;
+      } else {
+        this.y -= this.speed;
+      }
+    }
+    return newPlat;
+  },
 }
--- bundle.js
+++ bundle.js
@@ -93,14 +93,51 @@ platform = entity.newEntity();
 platform.color = "gray";
 
 module.exports = {
-  newPlatform: function(x,y,width,height){
+  newPlatform: function(x,y,width,height,color){
     var newPlat = Object.create(platform);
     newPlat.x = x;
     newPlat.y = y;
     newPlat.width = width;
     newPlat.height = height;
+    if(color){
+      newPlat.color = color;
+    }
     return newPlat;
-  }
+  },
+  newMovingPlatform: function(x,y,width,height,color, maxY, minY, speed){
+    var newPlat = Object.create(platform);
+    newPlat.x = x;
+    newPlat.y = y;
+    newPlat.maxY = maxY;
+    newPlat.minY = minY;
+    newPlat.width = width;
+    newPlat.height = height;
+    newPlat.goingDown = true;
+    if(color){
+      newPlat.color = color;
+    }
+    newPlat.speed = speed;
+    newPlat.update = function(){
+      if(this.y >= this.maxY) {
+        this.y = this.maxY;
+      }
+      if(this.y <= this.minY) {
+        this.y = this.minY;
+      }
+      if(this.y === this.maxY && this.goingDown === true) {
+        this.goingDown = false;
+      }
+      if(this.y === this.minY && this.goingDown === false) {
+        this.goingDown = true;
+      }
+      if(this.goingDown) {
+        this.y += this.speed;
+      } else {
+        this.y -= this.speed;
+      }
+    }
+    return newPlat;
+  },
 }
 
 },{"../Config/canvas":7,"./entity":1}],5:[function(require,module,exports){
@@ -597,9 +634,22 @@ var nextState = undefined;
 var gameLoop = undefined;
 
 var platforms = [];
+var movPlatforms = [];
+var lavas = [];
 var ladders = [];
 var particles = [];
 var player = undefined;
+
+function resetGame(){
+  clearInterval(gameLoop);
+  platforms = [];
+  movPlatforms = [];
+  lavas = [];
+  ladders = [];
+  player = undefined;
+  changeState = true;
+  nextState = "menuState";
+}
 
 function updateState(){
   player.update(config.getGravity());
@@ -678,6 +728,31 @@ function updateState(){
       player.whichPlatform = {};
     }
   }
+
+  for(var i = 0; i<movPlatforms.length;i++){
+    movPlatforms[i].update();
+    helpers.blockRect(player,movPlatforms[i]);
+    for(var j = 0; j<player.arrows.length;j++){
+      if(helpers.checkCollision(player.arrows[j],movPlatforms[i])){
+        particles.push(Particle.makeParticles(player.arrows[j].x,player.arrows[j].y,config.getGravity()));
+        player.arrows.splice(j,1);
+      }
+    }
+    /*if(helpers.checkCollision(player,movPlatforms[i])){
+      player.touchingPlatform = true;
+    }*/
+  }
+
+  for(var i = 0; i<lavas.length;i++){
+    if(helpers.checkCollision(player,lavas[i])){
+      resetGame();
+    }
+    for(var j = 0; j<player.arrows.length;j++){
+      if(helpers.checkCollision(player.arrows[j],lavas[i])){
+        player.arrows.splice(j,1);
+      }
+    }
+  }
 };
 
 module.exports = {
@@ -698,6 +773,9 @@ module.exports = {
     changeState = false;
     initialised = true;
     platforms = [];
+    movPlatforms = [];
+    lavas = [];
+    ladders = [];
     player = undefined;
 
     world = {
@@ -710,53 +788,76 @@ module.exports = {
     Screen.setScreen(0,world.height-c.height);
     gameScreen = Screen.getScreen();
 
-    platform1 = Platform.newPlatform(270,2620,228,12);
-    platform2 = Platform.newPlatform(200,2850,168,12);
-    platform3 = Platform.newPlatform(430,2750,128,12);
-    platform4 = Platform.newPlatform(40,2800,128,12);
-    platform5 = Platform.newPlatform(0,world.height-12,world.width,12);
-    platform6 = Platform.newPlatform(570,2520,268,12);
-    platform7 = Platform.newPlatform(990,2570,168,12);
-    platform8 = Platform.newPlatform(1270,2520,68,12);
-    platform9 = Platform.newPlatform(1290,2820,264,12);
-    platform10 = Platform.newPlatform(1670,2750,164,12);
-    platforms.push(platform1,platform2,platform3,platform4,platform5,platform6, platform7, platform8, platform9, platform10);
+    platform1 = Platform.newPlatform(270,2608,228,12);
+    platform2 = Platform.newPlatform(200,2838,168,12);
+    platform3 = Platform.newPlatform(430,2738,128,12);
+    platform4 = Platform.newPlatform(40,2788,128,12);
+    platform5 = Platform.newPlatform(0,world.height-24,200,12);
+    platform6 = Platform.newPlatform(1650,world.height-24,2500,12);
+    platform7 = Platform.newPlatform(570,2508,268,12);
+    platform8 = Platform.newPlatform(990,2552,168,12);
+    platform9 = Platform.newPlatform(1270,2508,68,12);
+    platform10 = Platform.newPlatform(1290,2808,264,12);
+    platform11 = Platform.newPlatform(1670,2738,164,12);
+    platforms.push(platform1,platform2,platform3,platform4,platform5,platform6, platform7, platform8, platform9, platform10,platform11);
 
-    ladder1 = Ladder.newLadder(100,2784,204);
-    ladder2 = Ladder.newLadder(450,2608,140);
-    ladder3 = Ladder.newLadder(1300,2508,312);
-    ladder4 = Ladder.newLadder(1700,2736,252);
+    movPlat1 = Platform.newMovingPlatform(400,world.height-36,70,12,'darkGreen',world.height-12, world.height-160,0.5);
+    movPlat2 = Platform.newMovingPlatform(570,world.height-80,70,12,'darkGreen',world.height-12, world.height-160,1);
+    movPlat3 = Platform.newMovingPlatform(740,world.height-12,70,12,'darkGreen',world.height-12, world.height-160,1.5);
+    movPlat4 = Platform.newMovingPlatform(910,world.height-100,70,12,'darkGreen',world.height-12, world.height-160,0.5);
+    movPlat5 = Platform.newMovingPlatform(1080,world.height-40,70,12,'darkGreen',world.height-12, world.height-160,0.2);
+    movPlat6 = Platform.newMovingPlatform(1250,world.height-20,70,12,'darkGreen',world.height-12, world.height-160,0.7);
+    movPlatforms.push(movPlat1,movPlat2,movPlat3,movPlat4,movPlat5,movPlat6);
+
+    lava1 = Platform.newPlatform(0,world.height-12,world.width,12,'darkOrange');
+    lavas.push(lava1);
+
+    ladder1 = Ladder.newLadder(100,2772,204);
+    ladder2 = Ladder.newLadder(450,2596,140);
+    ladder3 = Ladder.newLadder(1300,2496,312);
+    ladder4 = Ladder.newLadder(1700,2724,252);
     ladders.push(ladder1,ladder2,ladder3,ladder4);
 
     player = Player.getPlayer();
     player.y = world.height - player.height - 20;
+    player.x =30;
 
     gameLoop = setInterval(function(){
       updateState();
     },1000/config.getFPS());
   },
   draw: function() {
-    c.ctx.clearRect(0,0,c.width,c.height);
-    c.ctx.save();
-    c.ctx.translate(-gameScreen.x, -gameScreen.y);
-    for(var i = 0; i<platforms.length;i++){
-      c.ctx.fillStyle = platforms[i].color;
-      platforms[i].draw(c.ctx);
-    }
-    for(var i = 0; i<ladders.length;i++){
-      ladders[i].draw(c.ctx);
-    }
-    c.ctx.fillStyle = player.color;
-    player.draw(c.ctx);
-    c.ctx.fillStyle = '#8b0000';
-    for(var i = 0; i < particles.length; i++){
-      for(var j = 0; j < particles[i].length; j++){
-        if(!particles[i][j].dead){
-          particles[i][j].draw(c.ctx);
+    if(!changeState){
+      c.ctx.clearRect(0,0,c.width,c.height);
+      c.ctx.save();
+      c.ctx.translate(-gameScreen.x, -gameScreen.y);
+      for(var i = 0; i<platforms.length;i++){
+        c.ctx.fillStyle = platforms[i].color;
+        platforms[i].draw(c.ctx);
+      }
+      for(var i = 0; i<movPlatforms.length;i++){
+        c.ctx.fillStyle = movPlatforms[i].color;
+        movPlatforms[i].draw(c.ctx);
+      }
+      for(var i = 0; i<lavas.length;i++){
+        c.ctx.fillStyle = lavas[i].color;
+        lavas[i].draw(c.ctx);
+      }
+      for(var i = 0; i<ladders.length;i++){
+        ladders[i].draw(c.ctx);
+      }
+      c.ctx.fillStyle = player.color;
+      player.draw(c.ctx);
+      c.ctx.fillStyle = '#8b0000';
+      for(var i = 0; i < particles.length; i++){
+        for(var j = 0; j < particles[i].length; j++){
+          if(!particles[i][j].dead){
+            particles[i][j].draw(c.ctx);
+          }
         }
       }
+      c.ctx.restore()
     }
-    c.ctx.restore()
   },
 }
 
